496cdc4e72128cc1919d5ba91ad439b46bdce8bb
Taha Gargouri <taha.gargouri@itcexpertise.com>
2024-01-16 09:06:24 +0100

    search bar logic

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { SearchManufacturerProps } from '@/Types'
-import { Combobox  } from '@headlessui/react'
-import React from 'react'
+import { Combobox, Transition  } from '@headlessui/react'
+import React, { Fragment } from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
+import { manufacturers } from '@/constants'
 
 const SearchManufacturer = ({manufacturer,setManufacturer} : SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
   return (
-    <div className='search-manufacturer'>
+    <div className='m-10'>
       <Combobox> 
         <div className='relative w-full'> 
         <Combobox.Button className='absolute top-[14px]'> 
@@ -14,17 +17,25 @@ const SearchManufacturer = ({manufacturer,setManufacturer} : SearchManufacturerP
         src='/car-logo.svg'
         width={20}
         height={20}
-        className='ml-4'
+        className='ml-4' 
         alt='Car Logo' 
         />
          </Combobox.Button>
-         <Combobox.Input> 
-           
-         </Combobox.Input>
+         <Combobox.Input className="search-manufacturer_input"
+          placeholder='Volkswagen'
+          displayValue={(manufacturer:string)=> {return manufacturer}}
+          onChange={(e)=>setQuery(e.target.value)}/> 
+          <Transition as= {Fragment} 
+          leave='transition ease-in duration-100'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+          afterLeave={()=>setQuery('')}> 
+          <Combobox.Option> </Combobox.Option>
+
+          </Transition>
         </div>
       </Combobox>
     </div>
   )
 }
-
 export default SearchManufacturer
